cf57ed7e146010fce8e18349f808ec62ae44de84
Alexey Nabrovenkov <nabrovenkov@gmail.com>
2024-12-25 17:50:11 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import s from './styles.module.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const onButtonHandler = () => {
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className='App'>
+      <div className={s.countWrapper}>
+        <h1 className={s.name}>Счетчик:</h1>
+        <h2 className={s.count}>{count}</h2>
+        <div className={s.buttonWrapper}>
+          <button
+            className={`${s.button} + ${s.buttonMinus}`}
+            onClick={() => setCount((res) => res - 1)}
+          >
+            Минус
+          </button>
+          <button
+            className={`${s.button} + ${s.buttonPlus}`}
+            onClick={() => setCount((res) => res + 1)}
+          >
+            Плюс
+          </button>
+        </div>
+      </div>
+		</div>
+	);
 }
 
 export default App;
